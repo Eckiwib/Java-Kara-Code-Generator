d9e059aec1850f43a5c40fa059226043565278ec
eckiwib <eckhardwib@outlook.de>
2025-11-28 09:15:07 +0100

    selection system between 3 states with debug output

--- gui.py
+++ gui.py
@@ -10,15 +10,20 @@ root = tk.Tk()
 root.geometry(f"{w_size_x}x{w_size_y}")
 root.title("Java Kara Skript Generator")
 
+max = 3
+
 #grid Settings
-rows = 10
-cols = 10
+rows = 11
+cols = 11
 scattering = 30
 size = 30
-states = [[False for _ in range(rows)] for _ in range(cols)]
+states = [[0 for _ in range(rows)] for _ in range(cols)]
+colors = ["white","gray","green"]
 
-def toggle_bg(btn):
-    btn.config(bg="red")
+def toggle_bg(btn, row, col):
+    states[row][col] = (states[row][col]+1)%max
+    btn.config(bg=colors[states[row][col]%max])
+    print(f"Button: ({row}|{col}) is currently at state: {states[row][col]}")
 
 def Generate():
     pass
@@ -30,10 +35,10 @@ button.pack()
 x_align = (w_size_x-size*rows)/2
 y_align = (w_size_y-size*cols)/2
 
-for r in range(rows):
-    for c in range(cols):
-        grid_button = tk.Button(root)
-        grid_button.config(command=lambda b=grid_button: toggle_bg(b))
+for r in range(1,rows):
+    for c in range(1,cols):
+        grid_button = tk.Button(root, bg="white")
+        grid_button.config(command=lambda b=grid_button, x=r, y=c: toggle_bg(b, x, y))
         grid_button.place(x=r*scattering+x_align, y=c*scattering+y_align, width=size, height=size)
 
 root.mainloop()
