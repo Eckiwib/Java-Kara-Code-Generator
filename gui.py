68fc5334edc12b0b091416e7b9a9d84ccc205578
Eckiwib <eckhardwib@outlook.de>
2025-11-29 11:59:10 +0100

    working check system

--- gui.py
+++ gui.py
@@ -12,19 +12,21 @@ root.title("Java Kara Skript Generator")
 max = 3
 
 #grid Settings
-rows = 16
-cols = 16
+rows = 15
+cols = 15
 scattering = 30
 size = 30
 states = [[0 for _ in range(rows)] for _ in range(cols)]
 colors = ["white","gray","green"]
-ladybug = (8,2)
-
-preferl = True
+ladybug = (7,1)
 
 def toggle_bg(btn, row, col):
+
+    print(f"row: {row} column: {col}")
+    print(states[0][0])
+
     states[row][col] = (states[row][col]+1)%max
-    btn.config(bg=colors[states[row][col]%max])
+    btn.config(bg=colors[states[row][col]])
     print(f"Button: ({row}|{col}) is currently at state: {states[row][col]}")
 
 def generate():
@@ -37,8 +39,8 @@ button.pack()
 x_align = (w_size_x-size*rows)/2
 y_align = (w_size_y-size*cols)/2
 
-for r in range(1,rows):
-    for c in range(1,cols):
+for r in range(rows):
+    for c in range(cols):
         grid_button = tk.Button(root, bg="white")
         grid_button.config(command=lambda b=grid_button, x=r, y=c: toggle_bg(b, x, y))
         grid_button.place(x=r*scattering+x_align, y=c*scattering+y_align, width=size, height=size)
