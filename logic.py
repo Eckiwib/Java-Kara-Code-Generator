71e9e2653f77c7d93abbdc303a17aeb91f664773
Eckiwib <eckhardwib@outlook.de>
2025-11-29 17:40:42 +0100

    removed unnecessary stuff

--- logic.py
+++ logic.py
@@ -1,10 +1,7 @@
 Java_commands = {"move":"Kara.move()","Turn left":"Kara.turnLeft()","Turn right":"Kara.turnRight()"}
 
 def generate(states, ladybug):
-    rows = states.__len__()+1
-    cols = states[1].__len__()+1
 
-    facing = "down"
     ops_x = [1, -1, 0, 0]
     ops_y = [0, 0, 1, -1]
 
