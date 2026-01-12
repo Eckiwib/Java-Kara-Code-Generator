c6bb2d8c8342e224694590cbd352ad49cf0bd1b6
Eckiwib <eckhardwib@outlook.de>
2026-01-12 08:05:28 +0100

    sync

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import json
 
 with open("settings.json","r") as file:
     settings = json.load(file)
-    
+
 #windows config
 WIDTH = settings["window"]["width"]
 HEIGHT = settings["window"]["height"]
@@ -16,22 +16,28 @@ ROWS = GS["rows"]
 COLS = GS["columns"]
 SCATTERING = GS["scattering"]
 SIZE = GS["size"]
-STATES = [[0 for _ in range(ROWS)] for _ in range(COLS)]
 COLORS = GS["colors"]
 kara = settings["kara"]
+
+STATES = [[0 for _ in range(ROWS)] for _ in range(COLS)]
 
 max = 3
 
 def generate(states, kara):
     generator = logic.generate(states, kara)
-    for fac in range(4):
-        check = generator.check(fac)
-        if check > 0:
-            cmds = generator.fac_cmd()
-            generator.final()
-            break
-        
-    pyperclip.copy("\n".join(cmds))
+    fac = 0
+    cmds = []
+    while fac < 3:
+        for fac in range(4):
+            check = generator.check(fac)
+            print(check)
+            if check > 0:
+                cmds.append(generator.fac_cmd())
+                generator.final()
+                break
+    
+    print(cmds)
+    #pyperclip.copy("\n".join(cmds))
 
 window = gui.window(w_size_x=WIDTH, w_size_y=HEIGHT, size=SIZE,rows=ROWS, cols=COLS, scattering=SCATTERING, states=STATES, colors=COLORS, kara=kara, generate=generate)
 window.run()
